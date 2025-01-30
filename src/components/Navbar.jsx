ab95a2592ea939e901282814a510fecaa2c8d291
ypexxx <yua.alvin@gmail.com>
2025-01-30 16:06:54 +0700

    add logo website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,9 @@ function Navbar({filterData}) {
     return (
         <nav className="bg-slate-200">
             <div className="flex justify-between p-4">
-                <h1 className="inline font-bold">Note App</h1>
+                <div className="w-full flex items-center">
+                    <h1 className="font-bold text-xl text-[#000000]">Note App</h1>
+                </div>
                 <input type="text" placeholder="Cari..." onChange={onSearch} className="border-2 rounded-md border-slate-100 px-2"></input>
             </div>
         </nav>
